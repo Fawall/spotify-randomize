763b00972a7926b4710559fad8d3f8b439a0632d
Fabio Lopes <fabiolopes7844@gmail.com>
2024-01-19 17:37:22 -0300

    renamed file

--- src/connections.py
+++ src/connections.py
@@ -2,6 +2,7 @@ import os
 import json
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+from spotipy import SpotifyOAuth
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,10 +11,23 @@ load_dotenv()
 def connection():
     client_id = os.getenv("CLIENT_ID")
     client_secret = os.getenv("CLIENT_SECRET")
-
+    
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
                                                             client_secret=client_secret))
     return sp
 
+
+def create_playlist_connection():
+
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            scope="playlist-modify-public",
+            
+            client_id=os.getenv("CLIENT_ID"),
+            client_secret=os.getenv("CLIENT_SECRET"),
+            redirect_uri="https://google.com"    
+        )
+    )
+    return sp
 
 
